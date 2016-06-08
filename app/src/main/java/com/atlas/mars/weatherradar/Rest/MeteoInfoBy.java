fbb33932ab041a7a3aa41ce0291cefeeb26d8396
mars <lmaxim@mail.ru>
2016-06-08 18:31:00 +0300

    Обработка ошибки при обрыве интеренета

---
+++ app/src/main/java/com/atlas/mars/weatherradar/Rest/MeteoInfoBy.java
@@ -0,0 +1,7 @@
+package com.atlas.mars.weatherradar.Rest;
+
+/**
+ * Created by mars on 6/8/16.
+ */
+public class MeteoInfoBy {
+}
--- app/src/main/java/com/atlas/mars/weatherradar/Zoom/ActivityZoom.java
+++ app/src/main/java/com/atlas/mars/weatherradar/Zoom/ActivityZoom.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.atlas.mars.weatherradar.R;
 import com.atlas.mars.weatherradar.fragments.BitmapTransfer;
@@ -13,7 +15,9 @@ import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 /**
  * Created by mars on 5/14/16.
  */
-public class ActivityZoom extends Activity {
+public class ActivityZoom extends Activity implements View.OnClickListener{
+    ImageButton buttonPlay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +33,19 @@ public class ActivityZoom extends Activity {
         SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
         imageView.setMaxScale(4);
         imageView.setImage(ImageSource.bitmap(bitmap));
+
+        buttonPlay = (ImageButton)findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonPlay:
+                break;
+        }
+    }
+
 }
