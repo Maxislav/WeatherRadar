85e79566808dc6a6d584bdf76c0212a18163d334
mars <lmaxim@mail.ru>
2016-07-04 17:06:04 +0300

    drip detail forecast

--- app/src/main/java/com/atlas/mars/weatherradar/WeatherPager/ActivityFullWeatherInfo.java
+++ app/src/main/java/com/atlas/mars/weatherradar/WeatherPager/ActivityFullWeatherInfo.java
@@ -1,5 +1,6 @@
 package com.atlas.mars.weatherradar.WeatherPager;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerAdapter;
@@ -7,6 +8,7 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -70,10 +72,17 @@ public class ActivityFullWeatherInfo extends FragmentActivity implements Communi
     public void initView(View v, int position) {
         Log.d(TAG, "initView: " + position);
 
+       // inflateWebDrip(v)
+
+       // WebView browser = (WebView) v.findViewById(R.id.webViewDrips);
+        //browser.setBackgroundColor(Color.TRANSPARENT);
 
 
         TextView textViewTemp = (TextView)v.findViewById(R.id.textViewTemp);
         HashMap<String, String> map = list.get(position);
+
+        inflateWebDrip(v, map);
+
         textViewTemp.setText(list.get(position).get("temp").toString()+(char)0x00B0+"C");
         getIcon((ImageView)v.findViewById(R.id.image), map.get("icon").toString());
         ViewGroup viewGroup = (ViewGroup) v;
@@ -99,6 +108,85 @@ public class ActivityFullWeatherInfo extends FragmentActivity implements Communi
         }
 
     }
+
+    void inflateWebDrip(View view, final HashMap<String, String> map) {
+        WebView browser = (WebView) view.findViewById(R.id.webViewDrips);
+        browser.setBackgroundColor(Color.TRANSPARENT);
+        String drip = "";
+        boolean isNeedCreate = false;
+
+        String rain3h = null, snow3h = null;
+
+        if (map.get("rain") != null && !map.get("rain").isEmpty()) {
+            isNeedCreate = true;
+            rain3h = map.get("rain");
+            float f = Float.parseFloat(map.get("rain"));
+            f = f * 10;
+            for (int i = 0; i < f; i++) {
+                drip += "<img style=\"-webkit-user-select: none; width: 15px; height:24px; margin: 2px\" src=\"drip.png\">";
+            }
+
+
+            //  browser.loadDataWithBaseURL("file:///android_asset/", web, "text/html", "UTF-8", null);
+            //Todo касание и отображение тоста
+            /*browser.setOnTouchListener(new WebView.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    toast.show("Rain 3h: " + map.get("rain"), Gravity.TOP | Gravity.CLIP_HORIZONTAL);
+                    return false;
+                }
+            });*/
+        }
+
+        if (map.get("snow") != null && !map.get("snow").isEmpty()) {
+            isNeedCreate = true;
+            snow3h = map.get("snow");
+            float f = Float.parseFloat(map.get("snow"));
+            f = f * 10;
+            for (int i = 0; i < f; i++) {
+                drip += "<img style=\"-webkit-user-select: none; width: 22px; height:30px; margin: 4px 0px\" src=\"snow.png\">";
+            }
+        }
+
+        if (isNeedCreate) {
+
+          /*  String packageName = context.getPackageName();
+            int resId = context.getResources().getIdentifier("hh"+map.get("HH"), "color", packageName);
+            String color16 =    context.getString(resId);*/
+
+
+            String color16 = Forecast.getStringResourceByName("color", "hh" + map.get("HH"));
+            color16 = color16.replaceAll("^#.{2}", "#");
+
+
+            String web = "<html><head><meta name=\"viewport\" content=\"width=device-width, minimum-scale=0.1\"><title>drip.png (20×32)</title><style type=\"text/css\"></style></head><body style=\"margin: 0px;\">" +
+                    "<div style=\"position: relative; overflow: hidden; height: 120px; width: 100%; \">" +
+                    "<div>" + drip + "</div>" +
+                    "<div style=\"position: absolute; height: 120px; width: 100%; top: 0;left: 0; background:  linear-gradient(to bottom, rgba(0,0,0,0) 0%, " + color16 + " 100%);\">" +
+
+                    "</div>" +
+                    "</div>" +
+                    "</body></html>";
+            browser.loadDataWithBaseURL("file:///android_asset/", web, "text/html", "UTF-8", null);
+
+            String ts = "";
+
+            if (rain3h != null) {
+                ts += "Rain 3h: " + rain3h;
+            }
+            if (!ts.isEmpty()) {
+                ts += " ";
+            }
+
+            if (snow3h != null) {
+                ts += "Snow 3h: " + snow3h;
+            }
+            // onWebShowToast3h(browser, ts, map);
+           // view.findViewById(R.id.overLayout).setOnClickListener(new MyClick(ts));
+        }
+       // view.findViewById(R.id.overLayout).setOnLongClickListener(new MyLongClick(map, iDay));
+    }
+
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
